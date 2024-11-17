b64c73229449b72cb39c345b9355e05f5c6f05c8
Kazi <malikmutasim2121@gmail.com>
2024-11-17 07:50:10 +0600

    Added Employee and Department models and controllers to the YALB.Api

---
+++ YALB.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YALB.Api.Models;
+
+namespace YALB.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        public List<Department> departmentList = new List<Department>
+        {
+            new Department { Id = 1, Name = "IT" },
+            new Department { Id = 2, Name = "Finance" },
+            new Department { Id = 3, Name = "HR" }
+        };
+
+        [HttpGet]
+        public IActionResult GetDepartments()
+        {
+            return Ok(departmentList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDepartmentById(int id)
+        {
+            return Ok(departmentList.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
